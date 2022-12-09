f2b1fe856ab89064ee90314931ccc8128f42e8da
MinimalistPeach <stremler.lacika77@gmail.com>
2022-12-09 10:45:35 +0100

    wip

--- portfolio/src/components/PortfolioItem.jsx
+++ portfolio/src/components/PortfolioItem.jsx
@@ -8,11 +8,15 @@ function PortfolioItem({ title, imgUrl, stack, link }) {
          rel="noopener noreferrer"
          className="border-2 border-stone-900 dark:border-white rounded-md overflow-hidden"
       >
+         
+      <div className="w-64 bg-red-100 relative group">
+      <div class="opacity-0 group-hover:opacity-100 duration-100 absolute inset-x-0 text-center flex justify-center items-end bg-gray-200 text-black font-semibold">{title}</div>
          <img
             src={imgUrl}
-            alt="portfolio" 
-            className="w-full h-36 md:h-48 object-cover cursor-pointer"
+            alt={title}
+            className="w-full h-36 md:h-48 object-cover cursor-pointer blur-none hover:blur-sm"
          />
+         </div>
          <div className="w-full p-4">
             <h3 className="text-lg md:text-xl dark:text-white mb-2 md:mb-3 font-semibold ">{title}</h3>
             <p className="flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm dark:text-white ">
